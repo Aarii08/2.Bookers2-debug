ce131a2aa2124d1013cbb559eacaa8f5f5a733f2
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-08-19 12:44:37 +0000

    ヒント22まで終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,14 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @books = Book.all
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
       render "show"
     end
